4b1dbca08d0ef933587e8e4813f3415e2eb94b1f
BarbaraCipolla6 <128473393+BarbaraCipolla6@users.noreply.github.com>
2026-02-03 17:15:15 -0300

    Change server port from 3000 to 8080

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ app.use(express.json())
 // Routes
 app.use(noteRoutes)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
   console.log(`API corriendo en http://localhost:${PORT}`)
