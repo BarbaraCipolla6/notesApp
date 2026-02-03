1db2b49d72a8089a3f5ebaeb473c5636eba9888d
BarbaraCipolla6 <128473393+BarbaraCipolla6@users.noreply.github.com>
2026-02-03 17:22:38 -0300

    Change server listen address and log message

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,6 +13,6 @@ app.use(noteRoutes)
 
 const PORT = process.env.PORT || 8080
 
-app.listen(PORT, () => {
-  console.log(`API corriendo en http://localhost:${PORT}`)
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor escuchando en el puerto :${PORT}`)
 })
